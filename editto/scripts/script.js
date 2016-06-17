6f7132add9fa09fe3d99d91db41e562e98bb1598
gabrielkamimura <gabrielk.yano@gmail.com>
2016-06-17 12:02:01 -0300

    Fazendo editores diferentes funcionarem

--- editto/scripts/script.js
+++ editto/scripts/script.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
     var edicao = new eDitto("textExample");
-    var documento = edicao.obterDocumento(),
-        barraBotoes = edicao.obterBarraBotoes();
 
-    var editorInsercao = new eDitto("eDittoTextExample", {disableDefaultComponents: true});
+    var editorInsercao = new eDitto("eDittoTextExample", {disableDefaultComponents: true}),
+        documento1 = editorInsercao.obterDocumento();
     var personalizacao = new eDittoButton(new eDittoButtonGroup(editorInsercao.obterBarraBotoes()), 'smile-o', "Elemento personalizado");
     personalizacao.getButtonDOM().onclick = function() {
-      editorInsercao.obterDocumento().inserirTexto(":)");
+      documento1.inserirTexto(":)");
     };
 
-    var editorArquivo = new eDitto("eDittoFileLoadExample", {disableDefaultComponents: true});
+    var editorArquivo = new eDitto("eDittoFileLoadExample", {disableDefaultComponents: true}),
+        documento2 = editorInsercao.obterDocumento();
     var carregamento = new eDittoButton(new eDittoButtonGroup(editorArquivo.obterBarraBotoes()), 'random', "Modelo externo");
     carregamento.getButtonDOM().onclick = function() {
-      editorArquivo.obterDocumento().carregar('/editto/modelos/testeExterno.html');
+      documento2.carregar('/editto/modelos/testeExterno.html');
     };
 
-
-    var editorArqVars = new eDitto("eDittoFileVarsExample", {disableDefaultComponents: true});
+    var editorArqVars = new eDitto("eDittoFileVarsExample", {disableDefaultComponents: true}),
+        documento3 = editorInsercao.obterDocumento();
     var tpl = new eDittoButton(new eDittoButtonGroup(editorArqVars.obterBarraBotoes()), 'random', "Carregamento de template");
     tpl.getButtonDOM().onclick = function() {
       var opcoes = [
@@ -25,35 +25,37 @@ $(document).ready(function() {
           valor: prompt("Insira um título")
         }
       ];
-      editorArqVars.obterDocumento().carregar('/editto/modelos/testeTemplate.html', opcoes);
+      documento3.carregar('/editto/modelos/testeTemplate.html', opcoes);
     };
 
+    var documento = edicao.obterDocumento(),
+        barraBotoes = edicao.obterBarraBotoes();
     // Elementos personalizados
-    var buttonGroupCustom = new eDittoButtonGroup(barraBotoes);
-
-
-    var btnImagem = new eDittoButton(buttonGroupCustom, "picture-o", "Imagem");
-    btnImagem.getButtonDOM().onclick = function() {
-      var url = prompt("Insira a url da imagem");
-      if (url) {
-          var opcoes = [
-              {
-                  variavel: 'imagem.src',
-                  valor: url
-              }
-          ];
-          documento.carregar('modelos/imageInsert.html', opcoes);
-      }
-    };
-
-    var stgIns = new eDittoButton(buttonGroupCustom, 'comment', "Olá, meu amigo");
-    stgIns.getButtonDOM().onclick = function() {
-      var opcoes = [
-          {
-              variavel: 'nome',
-              valor: prompt("Insira o seu nome")
-          }
-      ];
-      documento.inserirComponente("<p>Olá, {{ nome }}!!</p>", opcoes);
-    }
+    // var buttonGroupCustom = new eDittoButtonGroup(barraBotoes);
+    //
+    //
+    // var btnImagem = new eDittoButton(buttonGroupCustom, "picture-o", "Imagem");
+    // btnImagem.getButtonDOM().onclick = function() {
+    //   var url = prompt("Insira a url da imagem");
+    //   if (url) {
+    //       var opcoes = [
+    //           {
+    //               variavel: 'imagem.src',
+    //               valor: url
+    //           }
+    //       ];
+    //       documento.carregar('modelos/imageInsert.html', opcoes);
+    //   }
+    // };
+    //
+    // var stgIns = new eDittoButton(buttonGroupCustom, 'comment', "Olá, meu amigo");
+    // stgIns.getButtonDOM().onclick = function() {
+    //   var opcoes = [
+    //       {
+    //           variavel: 'nome',
+    //           valor: prompt("Insira o seu nome")
+    //       }
+    //   ];
+    //   documento.inserirComponente("<p>Olá, {{ nome }}!!</p>", opcoes);
+    // }
 });
